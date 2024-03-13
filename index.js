49da8395d61bbca054f6446eaa573000823ef261
abdelhakim-ben <hakimbenslimane8@gmail.com>
2024-03-13 22:59:31 +0000

    add login

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const mineflayer=require('mineflayer')
-const bot = mineflayer.createBot({
+const botarg = {
     host: 'GesTamp.aternos.me',
     port: 64873,
     username: 'nero'
-})
+};
+
+let bot = mineflayer.createBot(botarg);
+    bot.on('login',()=> {
+    let botSocket = bot._client.socket;
+    console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
+
+});
+
+bot.on('end', () => {
+    console.log(`Disconnected`);
+});
+
