7ac7c5d5806e3bb034e69ea5a5f8c5314df40f32
abdelhakim-ben <hakimbenslimane8@gmail.com>
2024-03-13 23:09:18 +0000

    add reconnect

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const mineflayer=require('mineflayer')
-const botarg = {
+const botArgs = {
     host: 'GesTamp.aternos.me',
     port: 64873,
     username: 'nero'
 };
 
-let bot = mineflayer.createBot(botarg);
-    bot.on('login',()=> {
-    let botSocket = bot._client.socket;
-    console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
+const initBot = () => {
 
-});
+    // Setup bot connection
+    let bot = mineflayer.createBot(botArgs);
 
-bot.on('end', () => {
-    console.log(`Disconnected`);
-});
+    bot.on('login', () => {
+        let botSocket = bot._client.socket;
+        console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
+    });
+
+    bot.on('end', () => {
+        console.log(`Disconnected`);
+
+        // Attempt to reconnect
+        setTimeout(initBot, 5000);
+    });
+
+    bot.on('error', (err) => {
+        if (err.code === 'ECONNREFUSED') {
+            console.log(`Failed to connect to ${err.address}:${err.port}`)
+        }
+        else {
+            console.log(`Unhandled error: ${err}`);
+        }
+    });
+};
+
+initBot();
